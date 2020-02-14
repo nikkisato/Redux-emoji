0e14ea25cf1fd7c67721294f0322f0ff205a19bc
Nikki <nikki.satopdx@gmail.com>
2020-02-13 16:05:28 -0800

    almost all test are done

--- src/store.js
+++ src/store.js
@@ -3,6 +3,5 @@ import reducer from './reducers/moodReducers';
 
 export default createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
